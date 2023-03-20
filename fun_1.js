7c1ea03b07d2d4a645878e8f133deb4350165c52
Vlad <karabin0347@gmail.com>
2023-03-20 19:38:34 +0200

    HI

--- fun_1.js
+++ fun_1.js
@@ -3,3 +3,7 @@ function splitStringToWords(str) {
   const words = str.split(" ");
   return words;
 }
+
+const str = "   Hello world!    ";
+const words = splitStringToWords(str);
+console.log(words); // ["Hello", "world!"]
--- fun_5.js
+++ fun_5.js
@@ -2,4 +2,4 @@ function getDaysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
 
-console.log(getDaysInMonth(2, 2022));
+console.log(getDaysInMonth(4, 2022));
